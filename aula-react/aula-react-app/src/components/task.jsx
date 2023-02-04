4a85ee42e002a713d4585c3f8fb403bef79d8aea
Alex Rodrigues <alexmusicalsa@gmail.com>
2023-02-03 21:07:09 -0300

    Inicio do projeto

---
+++ aula-react/aula-react-app/src/components/task.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./task.css";
+
+const Task = ({ task, handleTaskClick }) => {
+  return (
+    <div
+      className="task-container"
+      style={task.complete ? { borderLeft: "6px solid chartreuse" } : {}}
+    >
+      <div className="task-title" onClick={() => handleTaskClick(task.id)}>
+        {task.title}
+      </div>
+    </div>
+  );
+
+  //   return <div className="task-container">{task.title}</div>;
+};
+
+export default Task;
